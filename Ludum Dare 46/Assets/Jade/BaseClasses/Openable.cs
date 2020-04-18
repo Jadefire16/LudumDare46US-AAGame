6b4281ecf3185b2709b3cb90a9abe81fe2340a29
Zach <53870574+Jadefire16@users.noreply.github.com>
2020-04-18 11:00:46 -0400

    Added Door stuff
    Swing them doors wide open bois

---
+++ Ludum Dare 46/Assets/Jade/BaseClasses/Openable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Openable : MonoBehaviour, IOpenable
+{
+
+    protected bool isOpen;
+
+    public virtual void OnClose() { isOpen = false; }
+    public virtual void OnOpen() { isOpen = true; }
+
+}
---
+++ Ludum Dare 46/Assets/Jade/DerivedClasses/Openable/Door.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider))]
+public class Door : Openable
+{
+    protected event Action OnObjOpenEvent;
+
+    Collider col;
+    float delayTime = 0.75f;
+    float currTime;
+    bool beginTimer = false;
+    protected void Start()
+    {
+        col = GetComponent<BoxCollider>(); // maybe make Get component in children
+        OnObjOpenEvent += OnOpen;
+    }
+
+    private void LateUpdate()
+    {
+        if(isOpen && beginTimer)
+        {
+            currTime += Time.deltaTime;
+        }
+
+        if (currTime >= delayTime)
+        {
+            OnClose();
+        }
+    }
+
+    public override void OnClose() 
+    {
+        base.OnClose();
+        currTime = 0f;
+        col.enabled = true;
+        beginTimer = false;
+        // Play Animation
+    }
+    public override void OnOpen() 
+    { 
+        base.OnOpen();
+        col.enabled = false;
+        //Play animation
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnObjOpenEvent?.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isOpen)
+            beginTimer = true;
+    }
+}
