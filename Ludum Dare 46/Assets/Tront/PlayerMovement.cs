1a32b55d9da63d84f908ff7526c3655f3e0ef3cd
Trent <trentsterling@gmail.com>
2020-04-18 18:11:46 -0500

    Sexy fire terrain and some other shit idk

---
+++ Ludum Dare 46/Assets/Tront/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    Vector3 inputVec = Vector3.zero;
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.z = Input.GetAxisRaw("Vertical");
+
+
+        inputVec = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * inputVec;
+
+
+
+        if (Input.GetButton("Jump"))
+        {
+
+            body.velocity = Vector3.up * 5f;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+        Vector3 velocity = -body.velocity;
+        velocity.y = 0;
+
+        body.AddForce(velocity * 0.2f, ForceMode.VelocityChange);
+
+        body.AddForce(inputVec * 100f);
+    }
+}
