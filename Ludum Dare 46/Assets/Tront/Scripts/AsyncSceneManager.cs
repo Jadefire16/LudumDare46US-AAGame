3e4f2f00267cc0f9534b20d181d2493f18e37766
Trent <trentsterling@gmail.com>
2020-04-19 00:55:14 -0500

    more async

--- Ludum Dare 46/Assets/Tront/Scripts/AsyncSceneManager.cs
+++ Ludum Dare 46/Assets/Tront/Scripts/AsyncSceneManager.cs
@@ -36,23 +36,25 @@ public class AsyncSceneManager : MonoBehaviour
         //Begin to load the Scene you specify
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
         //Don't let the Scene activate until you allow it to
-        asyncOperation.allowSceneActivation = false;
+        //asyncOperation.allowSceneActivation = false;
         Debug.Log("Pro :" + asyncOperation.progress);
         //When the load is still in progress, output the Text and progress bar
         while (!asyncOperation.isDone)
         {
             //Output the current progress
             text.text = "Loading progress: " + (asyncOperation.progress * 100) + "%";
+            yield return new WaitForEndOfFrame();
 
             // Check if the load has finished
             if (asyncOperation.progress >= 0.9f)
             {
+                // asyncOperation.allowSceneActivation = true;
                 //Change the Text to show the Scene is ready
-                text.text = "Press the space bar to continue";
+                text.text = "ALMOST THERE LOL";
                 //Wait to you press the space key to activate the Scene
-                if (Input.GetKeyDown(KeyCode.Space))
-                    //Activate the Scene
-                    asyncOperation.allowSceneActivation = true;
+                // if (Input.GetKeyDown(KeyCode.Space))
+                //Activate the Scene
+                //  asyncOperation.allowSceneActivation = true;
             }
 
             yield return null;
