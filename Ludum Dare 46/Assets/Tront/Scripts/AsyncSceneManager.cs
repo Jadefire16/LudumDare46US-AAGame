021759e6e79a2e57d30fb4c542c17d0b47f257db
Trent <trentsterling@gmail.com>
2020-04-19 00:19:01 -0500

    Added Text Mesh Pro and AsyncSceneManager

---
+++ Ludum Dare 46/Assets/Tront/Scripts/AsyncSceneManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AsyncSceneManager : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void LoadScene(string name)
+    {
+        if (name.Length > 0)
+        {
+            SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
+        }
+        else
+        {
+
+            if (sceneName.Length > 0)
+            {
+                SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            }
+            else
+            {
+                Debug.LogWarning("Bro you forgot to add a level name somewhere fuckin what are you doin");
+            }
+        }
+
+
+    }
+}
